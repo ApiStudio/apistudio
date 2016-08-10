673a863d61ff257b100ef0b12e0fe30799950a11
dewmal <dewmalnilanka@gmail.com>
2016-08-10 07:54:50 +0530

    Modular Creation Policy

---
+++ bootstrap/src/main/scala/io/egreen/apistudio/bootstrap/module/ModulRegistar.java
@@ -0,0 +1,7 @@
+package io.egreen.apistudio.bootstrap.module;
+
+/**
+ * Created by dewmal on 8/10/16.
+ */
+public class ModulRegistar {
+}
---
+++ monitor/src/main/java/io/egreen/apistudio/monitor/ApiStudioModule.java
@@ -0,0 +1,7 @@
+package io.egreen.apistudio.monitor;
+
+/**
+ * Created by dewmal on 8/10/16.
+ */
+public class ApiStudioModule {
+}
